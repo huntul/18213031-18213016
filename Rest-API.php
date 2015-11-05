6cc97881e5c892c06d1017b3d0c4a79d66362834
YuniCitrawati <ycitrawati@gmail.com>
2015-11-05 12:33:15 +0700

    Create Rest-API.php

---
+++ Rest-API.php
@@ -0,0 +1,25 @@
+<?php
+
+	function get_info_by_id($id) {
+		$info = array();
+
+		mysql_connect('localhost', 'root', '');
+		mysql_select_db('yuni');
+		$result = mysql_query('SELECT * FROM hp WHERE id = ' . $id);
+		$info = mysql_fetch_array ($result, MYSQL_ASSOC); 
+
+		return $info; 
+	}
+
+	if (isset($_GET["action"])) {
+		switch ($_GET["action"]) {
+			case "get_info";
+				if (isset($_GET["id"])) 
+					$value = get_info_by_id($_GET["id"]);
+				else
+					$value = "ERROR";
+				break;
+		}
+	}
+	exit(json_encode($value));	
+?>
